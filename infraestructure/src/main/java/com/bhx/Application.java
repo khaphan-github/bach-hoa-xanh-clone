41220e41db173de329f73a0b623eba028e22e7ca
khaphan-github <76431966+khaphan-github@users.noreply.github.com>
2023-05-14 09:57:18 +0700

    Change pom.xml duplicate lib thymleaf

--- infraestructure/src/main/java/com/bhx/Application.java
+++ infraestructure/src/main/java/com/bhx/Application.java
@@ -3,13 +3,16 @@ package com.bhx;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableAutoConfiguration
+@ComponentScan(basePackages = {
+        "com.bhx"
+})
 public class Application {
-
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
 }
